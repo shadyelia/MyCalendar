2499f5739c889de24fb55a18615afb80acac6b47
Shady Elia <shady.elia@hotmail.com>
2019-04-29 02:22:08 +0200

    finish auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
 
 @Component({
 	selector: 'app-root',
@@ -14,10 +16,26 @@ export class AppComponent implements OnInit {
 	constructor(
 		private platform: Platform,
 		private splashScreen: SplashScreen,
-		private statusBar: StatusBar
+		private statusBar: StatusBar,
+		private navCtrl: NavController,
+		private auth: AuthService
 	) {
 		this.initializeApp();
+
+		this.auth.afAuth.authState.subscribe(
+			user => {
+				if (user) {
+					this.navCtrl.navigateRoot('home');
+				} else {
+					this.navCtrl.navigateRoot('login');
+				}
+			},
+			() => {
+				this.navCtrl.navigateRoot('login');
+			}
+		);
 	}
+
 	initializeApp() {
 		this.platform.ready().then(() => {
 			this.statusBar.styleDefault();
@@ -46,6 +64,13 @@ export class AppComponent implements OnInit {
 	}
 
 	openPage(page) {
-		console.log(page);
+		if (page.title === 'الخروج') {
+			this.logout();
+		}
+	}
+
+	logout() {
+		this.auth.signOut();
+		this.navCtrl.navigateRoot('login');
 	}
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -29,4 +29,16 @@ export class AuthService {
 			credentials.password
 		);
 	}
+
+	get authenticated(): boolean {
+		return this.user !== null;
+	}
+
+	getEmail() {
+		return this.user && this.user.email;
+	}
+
+	signOut(): Promise<void> {
+		return this.afAuth.auth.signOut();
+	}
 }
